94bbf81b849fb7a1b842953b9b7b45172dfeaf4e
Tim Schwab <timothy.john.schwab@gmail.com>
2023-04-16 22:24:14 -0400

    just always build a sourcegraph - why not

--- web/build.ts
+++ web/build.ts
@@ -1,22 +1,13 @@
 import { build, stop } from 'esbuild/mod.js'
 
-let sourcemap;
-const mode = Deno.args[0];
-if (mode == "dev") {
-	sourcemap = true;
-} else if (mode == "prod") {
-	sourcemap = false;
-}
-
 const options = {
 	entryPoints: ['web/spa/app.ts'],
 	outdir: 'web/bundle',
 	target: 'esnext',
 	bundle: true,
 	minify: true,
-	sourcemap
+	sourcemap: true
 };
 
 await build(options);
-
 stop()
