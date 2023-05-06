155fa337df98e9af09668fa8560d767227f7f768
Tim Schwab <timothy.john.schwab@gmail.com>
2023-05-06 15:39:44 -0400

    fix camera

--- web/ts/camera.ts
+++ web/ts/camera.ts
@@ -1,4 +1,4 @@
-import { Circle, Point, Rect } from "../../common/data-types/structures.ts";
+import { Circle, Point, Rect, Vector } from "../../common/data-types/structures.ts";
 import canvas from "./canvas.ts";
 import { Color } from "./colors.ts";
 
@@ -9,31 +9,42 @@ function setCamera(rect: Rect) {
 }
 
 function fillScreen(color: Color) {
-	const screenTopLeft = new Point(0, 0);
-	const screenBottomRight = new Point(camera.width, camera.height);
-	const screenRect = new Rect(screenTopLeft, screenBottomRight);
+	const screenTopLeft = Point(0, 0);
+	const screenBottomRight = Point(camera.width, camera.height);
+	const screenRect = Rect(screenTopLeft, screenBottomRight);
 	canvas.fillRect(screenRect, color);
 }
 
 function outlineRect(rect: Rect, color: Color, lineWidth: number) {
-	const adjustedRect = rect.transpose(camera.topLeft);
+	const adjustedRect = transposeRect(rect, camera.topLeft);
 	canvas.outlineRect(adjustedRect, color, lineWidth);
 }
 
 function fillRect(rect: Rect, color: Color) {
-	const adjustedRect = rect.transpose(camera.topLeft);
+	const adjustedRect = transposeRect(rect, camera.topLeft);
 	canvas.fillRect(adjustedRect, color);
 }
 
 function line(start: Point, finish: Point, color: Color, lineWidth: number) {
-	const adjustedStart = start.transpose(camera.topLeft);
-	const adjustedFinish = finish.transpose(camera.topLeft);
+	const adjustedStart = transposePoint(start, camera.topLeft);
+	const adjustedFinish = transposePoint(finish, camera.topLeft);
 	canvas.line(adjustedStart, adjustedFinish, color, lineWidth);
 }
 
 function fillCircle(circle: Circle, color: Color) {
-	const adjustedCircle = circle.transpose(camera.topLeft);
+	const adjustedCircle = transposeCircle(circle, camera.topLeft);
 	canvas.fillCircle(adjustedCircle, color);
+}
+
+// Tranpose functions
+function transposePoint(original: Point, move: Vector): Point {
+	return Point(original.x-move.x, original.y-move.y);
+}
+function transposeRect(original: Rect, move: Vector) {
+	return Rect(transposePoint(original.topLeft, move), transposePoint(original.bottomRight, move));
+}
+function transposeCircle(original: Circle, move: Vector) {
+	return Circle(transposePoint(original.center, move), original.radius);
 }
 
 export default {
