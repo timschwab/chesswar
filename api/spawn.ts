f32fe67db15ea23fb9853da1f75efe7241996c64
Tim Schwab <timothy.john.schwab@gmail.com>
2023-06-04 12:20:25 -0400

    player drops what they are carrying upon death

--- api/spawn.ts
+++ api/spawn.ts
@@ -1,6 +1,9 @@
+import { CarryLoadType } from "../common/data-types/server.ts";
 import { Circle, Vector } from "../common/data-types/shapes.ts";
 import map from "../common/map.ts";
+import { ServerMessageTypes } from "../common/message-types/server.ts";
 import { gameEngine } from "../common/settings.ts";
+import socket from "./socket.ts";
 import { ServerPlayer } from "./state.ts";
 
 export function spawnPlayer(player: ServerPlayer): void {
@@ -17,4 +20,13 @@ export function spawnPlayer(player: ServerPlayer): void {
 		speed: Vector(0, 0),
 		position: Circle(start, radius)
 	};
+
+	player.carrying = {
+		type: CarryLoadType.EMPTY,
+		load: null
+	};
+	socket.sendOne(player.id, {
+		type: ServerMessageTypes.CARRYING,
+		payload: player.carrying
+	});
 }
