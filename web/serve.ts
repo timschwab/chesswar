d8c69f1421315ed3fbf2771f579247cf0004f685
Tim Schwab <timothy.john.schwab@gmail.com>
2023-06-29 14:23:26 -0400

    add content-type to the headers of the files served

--- web/serve.ts
+++ web/serve.ts
@@ -4,19 +4,27 @@ const handler = async (req: Request): Promise<Response> => {
 	const {pathname} = new URL(req.url);
 	console.log(pathname);
 
-	let file;
+	let file, contentType = "";
 	if (req.method == "GET" && pathname == "/") {
 		file = await Deno.open("web/static/index.html");
+		contentType = "text/html";
 	} else if (req.method == "GET" && pathname == "/main.css") {
 		file = await Deno.open("web/static/main.css");
+		contentType = "text/css";
 	}else if (req.method == "GET" && pathname == "/app.js") {
 		file = await Deno.open(buildSettings.bundleDir + "/app.js");
+		contentType = "application/javascript";
 	} else if (req.method == "GET" && pathname == "/app.js.map") {
 		file = await Deno.open(buildSettings.bundleDir + "/app.js.map");
+		contentType = "application/json";
 	}
 
 	if (file) {
-		return new Response(file.readable);
+		return new Response(file.readable, {
+			headers: {
+				"Content-Type": contentType
+			}
+		});
 	} else {
 		return new Response(null, {
 			status: 404
