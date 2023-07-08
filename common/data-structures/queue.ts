51f2fc3c80849648fc5894f0df98dfeb911d322c
Tim Schwab <timothy.john.schwab@gmail.com>
2023-07-07 23:20:21 -0400

    an interesting abstraction

---
+++ common/data-structures/queue.ts
@@ -0,0 +1,40 @@
+export class Queue<T> {
+	values: Map<number,T>;
+	head: number;
+	tail: number;
+
+	constructor() {
+		this.values = new Map<number, T>();
+		this.head = 0;
+		this.tail = 0;
+	}
+
+	enqueue(value: T): void {
+		this.values.set(this.tail, value);
+		this.tail++;
+	}
+
+	dequeue(): T | null {
+		const value = this.values.get(this.head);
+		if (value === undefined) {
+			return null;
+		} else {
+			this.values.delete(this.head);
+			this.head++;
+			return value;
+		}
+	}
+
+	peek(): T | null {
+		const value = this.values.get(this.head);
+		if (value === undefined) {
+			return null;
+		} else {
+			return value;
+		}
+	}
+
+	length(): number {
+		return this.values.size;
+	}
+}
--- web/ts/state2.ts
+++ web/ts/state2.ts
@@ -1,1 +1,42 @@
-// Hmmmmmm...
+import { Queue } from "../../common/data-structures/queue.ts";
+
+interface Diff<T> {
+	prev: T,
+	cur: T
+}
+
+export function makeStore<T>(initialValue: T) {
+	let previousValue: T;
+	let currentValue: T = initialValue;
+	const diffQueue: Queue<Diff<T>> = new Queue<Diff<T>>();
+
+	const getPreviousValue = function(): T {
+		return previousValue;
+	}
+
+	const getCurrentValue = function(): T {
+		return currentValue;
+	}
+
+	const setValue = function(newValue: T): void {
+		previousValue = currentValue;
+		currentValue = newValue;
+
+		const diff: Diff<T> = {
+			prev: previousValue,
+			cur: newValue
+		};
+		diffQueue.enqueue(diff);
+	}
+
+	const nextDiff = function(): Diff<T> | null {
+		return diffQueue.dequeue();
+	}
+
+	return {
+		getPreviousValue,
+		getCurrentValue,
+		setValue,
+		nextDiff
+	};
+}
