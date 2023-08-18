54ca28df31b96bec8737d3b0408eaff64af6b98a
Tim Schwab <timothy.john.schwab@gmail.com>
2023-08-17 22:38:45 -0400

    extract clicking

--- web/ts/clientGame.ts
+++ web/ts/clientGame.ts
@@ -2,12 +2,10 @@ import socket from "./socket.ts";
 import state, { isSafeState } from "./state.ts";
 import { renderAll } from "./render/renderAll.ts";
 import { ServerMessage, ServerMessageTypes } from "../../common/message-types/server.ts";
-import { PlayerRole } from "../../common/data-types/base.ts";
 import { listenClick } from "./inputs.ts";
-import { clickedButton, clickedSquare } from "./generalWindow.ts";
 import { ClientMessageTypes } from "../../common/message-types/client.ts";
 import { handleCarrying, handleCompletedAction, handleDeath, handlePlayerInit, handlePong, handleState, handleStats, handleTeam } from "./messages.ts";
-import { Point } from "../../common/shapes/Point.ts";
+import { receiveClick } from "./click.ts";
 
 export function initGame() {
 	socket.listen(receiveMessage);
@@ -32,45 +30,6 @@ function receiveMessage(message: ServerMessage): void {
 		handlePong();
 	} else if (message.type == ServerMessageTypes.STATS) {
 		handleStats(message.payload);
-	}
-}
-
-function receiveClick(location: Point): void {
-	if (!isSafeState(state)) {
-		return;
-	}
-
-	if (state.self.role != PlayerRole.GENERAL) {
-		return;
-	}
-
-	const button = clickedButton(state, location);
-	const square = clickedSquare(state, location);
-	if (button != null) {
-		state.general.selectedButton = button;
-		state.general.selectedFrom = null;
-	} else if (state.general.selectedButton != null && square != null) {
-		if (state.general.selectedFrom) {
-			// Send orders
-			const payload = {
-				briefing: state.general.selectedButton,
-				move: {
-					team: state.self.team,
-					from: state.general.selectedFrom,
-					to: square
-				}
-			};
-			socket.send({
-				type: ClientMessageTypes.GENERAL_ORDERS,
-				payload
-			});
-
-			// Clear state
-			state.general.selectedButton = null;
-			state.general.selectedFrom = null;
-		} else {
-			state.general.selectedFrom = square;
-		}
 	}
 }
 
