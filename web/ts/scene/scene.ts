7e00538aa4b38c761b2836137152ac169af7ceef
Tim Schwab <timothy.john.schwab@gmail.com>
2023-09-05 00:10:44 -0400

    add all rects back

--- web/ts/scene/scene.ts
+++ web/ts/scene/scene.ts
@@ -1,5 +1,6 @@
 import map from "../../../common/map.ts";
 import { rensets } from "../../../common/settings.ts";
+import { Circle } from "../../../common/shapes/Circle.ts";
 import { Point } from "../../../common/shapes/Point.ts";
 import { Rect } from "../../../common/shapes/Rect.ts";
 import { gameRoot } from "../canvas/dom.ts";
@@ -34,4 +35,82 @@ for (let y = 0; y <= map.height; y += rensets.grid.spacing) {
 		color: rensets.grid.color
 	};
 	horizontalGrid.addStaticRect(shape);
+}
+
+// Map external areas
+const externalMap = scene.createLayer();
+for (const bundle of map.facilities) {
+	externalMap.addStaticRect({
+		geo: bundle.base,
+		color: rensets.facilities.ally.base
+	});
+
+	for (const outpost of bundle.outposts) {
+		externalMap.addStaticRect({
+			geo: outpost, color: rensets.facilities.ally.outpost
+		});
+	}
+}
+
+// Map internal areas
+const internalMap = scene.createLayer();
+for (const bundle of map.facilities) {
+	internalMap.addStaticRect({
+		geo: bundle.command, color: rensets.facilities.ally.command
+	});
+
+	for (const briefing of bundle.briefings) {
+		internalMap.addStaticRect({
+			geo: briefing, color: rensets.facilities.ally.pickup
+		});
+	}
+
+	internalMap.addStaticRect({
+		geo: bundle.armory, color: rensets.facilities.ally.armory
+	});
+
+	internalMap.addStaticRect({
+		geo: bundle.scif, color: rensets.facilities.ally.scif
+	});
+}
+
+// Minefields
+const minefields = scene.createLayer();
+for (const minefield of map.minefields) {
+	if (Rect.isRect(minefield)) {
+		minefields.addStaticRect({
+			geo: minefield, color: rensets.minefield.color
+		});
+	} else if (Circle.isCircle(minefield)) {
+		//circles.push([minefield, rensets.minefield.color]);
+	} else {
+		// Can't get here
+	}
+}
+
+// Draw safe zone and battlefield
+//circles.push([map.safeZone, rensets.center.safe]);
+//circles.push([map.battlefield, rensets.center.battlefield]);
+
+// Map boundaries
+const outer = map.shape.geo.expand(rensets.mapBorder.width/2);
+const inner = map.shape.geo.shrink(rensets.mapBorder.width/2);
+const overlaps = outer.overlap(inner);
+
+if (overlaps.first.left && overlaps.first.right && overlaps.first.top && overlaps.first.bottom) {
+	const verticalBoundaries = scene.createLayer();
+	verticalBoundaries.addStaticRect({
+		geo: overlaps.first.left, color: rensets.mapBorder.color
+	});
+	verticalBoundaries.addStaticRect({
+		geo: overlaps.first.right, color: rensets.mapBorder.color
+	});
+
+	const horizontalBoundaries = scene.createLayer();
+	horizontalBoundaries.addStaticRect({
+		geo: overlaps.first.top, color: rensets.mapBorder.color
+	});
+	horizontalBoundaries.addStaticRect({
+		geo: overlaps.first.bottom, color: rensets.mapBorder.color
+	});
 }
