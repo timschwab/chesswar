f5cd5303e00a8d7dc9f336baf479e1c6690291d8
Tim Schwab <timothy.john.schwab@gmail.com>
2023-12-02 17:39:01 -0600

    just 10, and new ones each frame

--- web/ts/clientGame.ts
+++ web/ts/clientGame.ts
@@ -34,45 +34,53 @@ gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 // Set the viewport
 gl.viewport(0, 0, width, height);
 
-// Clear the canvas
-gl.clearColor(0, 0, 0, 0);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
 // Tell it to use our program (pair of shaders)
 gl.useProgram(program);
 
 // set the resolution
 gl.uniform2f(resolutionUniformLocation, width, height);
 
-// Prep the position attribute
-gl.enableVertexAttribArray(positionAttributeLocation);
+// End of real initialization code, real beginning of render code
 
-// Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-const size = 2;          // 2 components per iteration
-const type = gl.FLOAT;   // the data is 32bit floats
-const normalize = false; // don't normalize the data
-const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-const attribOffset = 0;  // start at the beginning of the buffer
-gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, attribOffset)
-
-// draw random triangles in random colors
-for (let ii = 0; ii < 1000; ++ii) {
-	const positions = [
-		randomInt(width), randomInt(height),
-		randomInt(width), randomInt(height),
-		randomInt(width), randomInt(height),
-	];
-
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-	// Set a random color.
-	gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
-
-	// Draw the triangle
-	gl.drawArrays(gl.TRIANGLES, 0, 3);
-}
+requestAnimationFrame(() => drawTriangles(gl));
 
 // Functions
+
+function drawTriangles(gl: WebGLRenderingContext) {
+	requestAnimationFrame(() => drawTriangles(gl));
+
+	// Clear the canvas
+	gl.clearColor(0, 0, 0, 0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	// Prep the position attribute
+	gl.enableVertexAttribArray(positionAttributeLocation);
+
+	// Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+	const size = 2;          // 2 components per iteration
+	const type = gl.FLOAT;   // the data is 32bit floats
+	const normalize = false; // don't normalize the data
+	const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+	const attribOffset = 0;  // start at the beginning of the buffer
+	gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, attribOffset)
+
+	// draw random triangles in random colors
+	for (let ii = 0; ii < 10; ++ii) {
+		const positions = [
+			randomInt(width), randomInt(height),
+			randomInt(width), randomInt(height),
+			randomInt(width), randomInt(height),
+		];
+
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+		// Set a random color.
+		gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+
+		// Draw the triangle
+		gl.drawArrays(gl.TRIANGLES, 0, 3);
+	}
+}
 
 // Returns a random integer in [0, range)
 function randomInt(range: number) {
