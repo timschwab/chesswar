e47f6ff714bf85399b6b97a446f824b7c60a8631
Tim Schwab <timothy.john.schwab@gmail.com>
2023-12-02 17:07:20 -0600

    random squares

--- web/ts/clientGame.ts
+++ web/ts/clientGame.ts
@@ -20,24 +20,13 @@ const program = createProgram(gl, vertexShader, fragmentShader);
 // Grab locations
 const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+const colorUniformLocation = gl.getUniformLocation(program, "u_color");
 
 // Create buffers
 const positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-// Actually load the data
-const positions = [
-	10, 20,
-	80, 20,
-	10, 30,
-
-	10, 30,
-	80, 20,
-	80, 30,
-];
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-// Render
+// End of initialization code, beginning of render code
 
 // Set the viewport
 gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -55,9 +44,6 @@ gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 // Prep the position attribute
 gl.enableVertexAttribArray(positionAttributeLocation);
 
-// Bind the position buffer.
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
 // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
 const size = 2;          // 2 components per iteration
 const type = gl.FLOAT;   // the data is 32bit floats
@@ -66,8 +52,49 @@ const stride = 0;        // 0 = move forward size * sizeof(type) each iteration 
 const attribOffset = 0;        // start at the beginning of the buffer
 gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, attribOffset)
 
-// Draw!
-const primitiveType = gl.TRIANGLES;
-const arraysOffset = 0;
-const count = 6;
-gl.drawArrays(primitiveType, arraysOffset, count);
+// draw 50 random rectangles in random colors
+for (let ii = 0; ii < 50; ++ii) {
+	// Setup a random rectangle
+	// This will write to positionBuffer because
+	// its the last thing we bound on the ARRAY_BUFFER
+	// bind point
+	setRectangle(
+		gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
+
+	// Set a random color.
+	gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+
+	// Draw the rectangle.
+	gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
+
+// Functions
+
+// Returns a random integer from 0 to range - 1.
+function randomInt(range: number) {
+	return Math.floor(Math.random() * range);
+}
+
+// Fills the buffer with the values that define a rectangle.
+function setRectangle(gl: WebGLRenderingContext, x: number, y: number, width: number, height: number) {
+	const x1 = x;
+	const x2 = x + width;
+	const y1 = y;
+	const y2 = y + height;
+
+	// NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
+	// whatever buffer is bound to the `ARRAY_BUFFER` bind point
+	// but so far we only have one buffer. If we had more than one
+	// buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
+
+	const positions = [
+		x1, y1,
+		x2, y1,
+		x1, y2,
+		x1, y2,
+		x2, y1,
+		x2, y2
+	];
+
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+}
--- web/ts/fragmentShader.ts
+++ web/ts/fragmentShader.ts
@@ -3,11 +3,13 @@ const shader = `
 // fragment shaders don't have a default precision so we need
 // to pick one. mediump is a good default. It means "medium precision"
 precision mediump float;
+
+uniform vec4 u_color;
  
 void main() {
 	// gl_FragColor is a special variable a fragment shader
 	// is responsible for setting
-	gl_FragColor = vec4(1, 0, 0.5, 1); // return reddish-purple
+	gl_FragColor = u_color;
 }
 
 `;
