6ff26e5992950aa36871cf38d9b1494a9568bca8
Tim Schwab <timothy.john.schwab@gmail.com>
2024-01-19 21:09:22 -0500

    first non letter heck yea

--- web/ts/text/symbolMappings.ts
+++ web/ts/text/symbolMappings.ts
@@ -3,5 +3,9 @@ import { TriangleVerticesArray } from "../../../common/shapes/Triangle.ts";
 export const symbolMappings: Record<string, TriangleVerticesArray[]> = {
 	" ": [
 		// Empty of course!
+	],
+	"|": [
+		[0.4, -0.3, 0.6, -0.3, 0.4, 1.3],
+		[           0.6, -0.3, 0.4, 1.3, 0.6, 1.3],
 	]
 };
