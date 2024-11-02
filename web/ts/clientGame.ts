867f4292499d158646662e7f8d1a663434b08eee
Tim Schwab <timothy.john.schwab@gmail.com>
2024-11-02 00:58:36 -0400

    class for an expanding text texture

--- web/ts/clientGame.ts
+++ web/ts/clientGame.ts
@@ -1,4 +1,5 @@
 import { getAttachedCanvas, getCanvas } from "./core/dom.ts";
+import { ExpandingTextCanvas } from "./text/ExpandingTextCanvas.ts";
 
 initGame();
 
@@ -15,53 +16,5 @@ function gameLoop() {
 	// requestAnimationFrame(gameLoop);
 
 	// Get the fake canvas
-	const canvas = getAttachedCanvas();
-	const context = canvas.getContext("2d");
-	if (context === null) {
-		return;
-	}
-
-	// Font settings
-	const fontFamily = "Courier New";
-	const fontHeight = "128px";
-	const font = `${fontHeight} ${fontFamily}`;
-
-	// Set initial values for the canvas
-	context.fillStyle = "white";
-	context.textAlign = "left";
-	context.textBaseline = "top";
-	context.font = font;
-
-	// Get metrics on this browser's implementation of the font
-	const metrics = context.measureText(".");
-	const letterHeight = Math.ceil(metrics.actualBoundingBoxDescent-metrics.actualBoundingBoxAscent);
-	const oneLetterWidth = Math.ceil(metrics.actualBoundingBoxRight-metrics.actualBoundingBoxLeft);
-
-	// Write the first letter
-	canvas.width = oneLetterWidth;
-	canvas.height = letterHeight;
-
-	context.fillStyle = "white";
-	context.textAlign = "left";
-	context.textBaseline = "top";
-	context.font = font;
-
-	context.fillText("A", oneLetterWidth*0, 0);
-
-	// Expand the canvas to fit the second letter
-	const savedData = context.getImageData(0, 0, oneLetterWidth, letterHeight);
-	canvas.width = oneLetterWidth*2;
-	context.putImageData(savedData, 0, 0);
-
-	// Write the second letter
-	context.fillStyle = "white";
-	context.textAlign = "left";
-	context.textBaseline = "top";
-	context.font = font;
-
-	context.fillText("B", oneLetterWidth*1, 0);
-
-	//const dataUrl = canvas.toDataURL();
-	//const image = new Image();
-	//image.src = dataUrl;
+	const expandingTexture = new ExpandingTextCanvas();
 }
---
+++ web/ts/text/ExpandingTextCanvas.ts
@@ -0,0 +1,85 @@
+import { Point } from "../../../common/shapes/Point.ts";
+import { Rect } from "../../../common/shapes/Rect.ts";
+import { ZeroPoint } from "../../../common/shapes/Zero.ts";
+import { getCanvas } from "../core/dom.ts";
+
+// Font settings
+const FONT_FAMILY = "Courier New";
+const FONT_HEIGHT = "128px";
+const FONT = `${FONT_HEIGHT} ${FONT_FAMILY}`;
+
+export class ExpandingTextCanvas {
+	private readonly canvas = getCanvas();
+	private readonly context = this.getContext();
+	private readonly letterBoundingBox = this.getLetterBoundingBox();
+	private letterCount = 0;
+
+	constructor() {
+		// Set initial values and flags
+		this.canvas.width = 0;
+		this.canvas.height = this.letterBoundingBox.height;
+	}
+
+	private getContext(): CanvasRenderingContext2D {
+		const context = this.canvas.getContext("2d", {
+			willReadFrequently: true // Optimization flagged to me by Chrome
+		});
+		if (context === null) {
+			throw "Could not get canvas context";
+		} else {
+			return context;
+		}
+	}
+
+	private getLetterBoundingBox(): Rect {
+		// Set initial values for the canvas
+		this.setStyleValues();
+	
+		// Get metrics on this browser's implementation of the font
+		const metrics = this.context.measureText(".");
+		const oneLetterWidth = Math.ceil(metrics.actualBoundingBoxRight-metrics.actualBoundingBoxLeft);
+		const oneLetterHeight = Math.ceil(metrics.actualBoundingBoxDescent-metrics.actualBoundingBoxAscent);
+	
+		return new Rect(ZeroPoint, new Point(oneLetterWidth, oneLetterHeight));
+	}
+
+	private setStyleValues(): void {
+		this.context.fillStyle = "white";
+		this.context.textAlign = "left";
+		this.context.textBaseline = "top";
+		this.context.font = FONT;
+	}
+
+	addLetter(letter: string): number {
+		// Validate it is only one letter
+		if (letter.length !== 1) {
+			throw "Expected exactly one letter but found " + letter.length + " instead.";
+		}
+
+		// Compute vars
+		const previousWidth = this.canvas.width;
+		const newWidth = previousWidth + this.letterBoundingBox.width;
+
+		// Expand canvas to fit the new letters
+		if (this.letterCount > 0) {
+			const savedData = this.context.getImageData(0, 0, previousWidth, this.canvas.height);
+			this.canvas.width = newWidth;
+			this.context.putImageData(savedData, 0, 0);
+		} else {
+			this.canvas.width = newWidth;
+		}
+
+		// Write the new letter
+		this.setStyleValues();
+		this.context.fillText(letter, previousWidth, 0);
+
+		// Return the new index
+		return this.letterCount++;
+	}
+
+	getTexture(): HTMLImageElement {
+		const image = new Image();
+		image.src = this.canvas.toDataURL();
+		return image;
+	}
+}
