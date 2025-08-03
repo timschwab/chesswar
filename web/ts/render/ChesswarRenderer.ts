63dc68fd91951e05e1b0fe4062e0408369ad34b6
Tim Schwab <timothy.john.schwab@gmail.com>
2025-08-03 15:07:35 -0400

    make better expanding glyph texture

--- web/ts/render/ChesswarRenderer.ts
+++ web/ts/render/ChesswarRenderer.ts
@@ -4,6 +4,7 @@ import { recordJsRenderTime, recordTimeBetweenAnimations } from "../game-logic/s
 import { MapRenderer } from "../webgl/map/MapRenderer.ts";
 import { PlayerRenderer } from "../webgl/player/PlayerRenderer.ts";
 import { RectangleRenderer } from "../webgl/rectangle/RectangleRenderer.ts";
+import { ExpandingGlyphTexture } from "../webgl/text/ExpandingGlyphTexture.ts";
 import { TeamRoleRenderer } from "./UserInterface/TeamRoleRenderer.ts";
 
 export class ChesswarRenderer {
@@ -20,6 +21,14 @@ export class ChesswarRenderer {
 		this.playerRenderer = new PlayerRenderer();
 
 		const rectangleRenderer = new RectangleRenderer();
+		const textRenderer = new ExpandingGlyphTexture();
+
+		textRenderer.addGrapheme("0");
+		textRenderer.addGrapheme("1");
+		textRenderer.addGrapheme("2");
+		textRenderer.addGrapheme("3");
+		textRenderer.addGrapheme("4");
+
 		this.teamRoleRenderer = new TeamRoleRenderer(rectangleRenderer);
 		//this.chessboardRenderer = new ChessboardRenderer();
 	}
